dd9a9afe5d3d1b039d84ed7086acf38739f94d76
trxgtradingpro-jpg <trxtradingpro@gmail.com>
2026-02-02 01:17:12 -0300

    Update app.py

--- app.py
+++ app.py
@@ -89,7 +89,7 @@ PLANOS = {
     "trx-teste": {
         "nome": "TRX TESTE",
         "pasta": "Licencas/TRX TESTE",
-        "preco": 100,
+        "preco": 110,
         "redirect_url": "https://sites.google.com/view/planogratuito/in%C3%ADcio"
     },
     "trx-gratis": {
@@ -330,5 +330,6 @@ def admin_pedido(order_id):
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
 
 
